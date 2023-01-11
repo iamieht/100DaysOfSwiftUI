74347118af50421635ec217a0e8a2405e659a239
Iván E. Hernández T <iamieht@gmail.com>
2023-01-11 17:12:24 +0100

    Day 2

---
+++ MyPlayground.playground/Pages/Checkpoints.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+// Checkpoint 1
+let tempCelsius = 0.0
+let tempFahrenheit = tempCelsius * 9 / 5 + 32
+print("\(tempCelsius)C° is \(tempFahrenheit)F°")
--- MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
@@ -59,3 +59,34 @@ print(number.isMultiple(of: 3))
 let number2 = 0.1 + 0.2
 print(number2)
 
+// Day 2
+// Booleans
+let goodDogs = true
+var gameOver = false
+
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+// How to join strings together
+let firstPart = "Hello, "
+let secondPart = "world!"
+greeting = firstPart + secondPart
+print(greeting)
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+
+name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+print("5 x 5 is \(5*5)")
