7e8ab47c39e0bfda294094e6bac6738f66f2c993
Iván E. Hernández T <iamieht@gmail.com>
2023-01-12 19:18:39 +0100

    Day 3

--- MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
@@ -90,3 +90,46 @@ let message = "Hello, my name is \(name) and I'm \(age) years old."
 print(message)
 
 print("5 x 5 is \(5*5)")
+
+// Day 3
+// Arrays
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4,8,15,16,23,42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+beatles.append("Adrian")
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(85)
+
+print(scores)
+
+var albums = [String]()
+albums.append("Folklore")
+albums.append("Fearless")
+
+print(albums.count)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+
+characters.remove(at: 2)
+print(characters.count)
+
+characters.removeAll()
+print(characters.count)
+
+let bondMovies = ["Casino Royale", "Spectre", "No Time to Die"]
+print(bondMovies.contains("Frozen"))
+
+let cities = ["London", "Tokio", "Rome", "Budapest"]
+print(cities.sorted())
+
+let presidents = ["Bush", "Obama", "Trump", "Biden"]
+let reversedPresidents = presidents.reversed()
