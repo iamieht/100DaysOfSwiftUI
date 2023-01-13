1787ef7d716c7343fe4377ac343d8ae0bee68d04
Iván E. Hernández T <iamieht@gmail.com>
2023-01-13 07:54:46 +0100

    Day 4 - Checkpoint 2

--- MyPlayground.playground/Pages/Checkpoints.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Checkpoints.xcplaygroundpage/Contents.swift
@@ -4,3 +4,9 @@ import Cocoa
 let tempCelsius = 0.0
 let tempFahrenheit = tempCelsius * 9 / 5 + 32
 print("\(tempCelsius)C° is \(tempFahrenheit)F°")
+
+// Checkpoint 2
+let countries = ["Venezuela", "USA", "England", "Germany", "Spain", "Australia", "Spain"]
+print("There are \(countries.count)  countries in the array ")
+let uniqueCountries = Set(countries)
+print("There are \(uniqueCountries.count) unique countries in the array")
