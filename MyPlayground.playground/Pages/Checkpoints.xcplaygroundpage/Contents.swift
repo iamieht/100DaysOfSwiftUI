f692e8a4fbaae9ccc9765b7f5d3451db8647d752
Iván E. Hernández T <iamieht@gmail.com>
2023-01-15 08:44:22 +0100

    Day 6 checkpoint 3

--- MyPlayground.playground/Pages/Checkpoints.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Checkpoints.xcplaygroundpage/Contents.swift
@@ -10,3 +10,17 @@ let countries = ["Venezuela", "USA", "England", "Germany", "Spain", "Australia",
 print("There are \(countries.count)  countries in the array ")
 let uniqueCountries = Set(countries)
 print("There are \(uniqueCountries.count) unique countries in the array")
+
+// Checkpoint 3 ("FizzBuzz")
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
