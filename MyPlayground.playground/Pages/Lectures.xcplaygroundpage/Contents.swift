787f6b2050922c4d5101325dcd695d27337a40cd
Iván E. Hernández T <iamieht@gmail.com>
2023-01-18 18:58:12 +0100

    Day 10: property observers

--- MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
@@ -660,3 +660,37 @@ var archer2 = Employee2(name: "Sterling Archer")
 archer2.vacationTaken += 4
 archer2.vacationRemaining = 5
 print(archer2.vacationAllocated)
+
+// property observers
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts")
+            print("Old value was: \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
