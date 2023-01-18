869037f650c5a0f46f2ef4a71f0c0eaf5e72d196
Iván E. Hernández T <iamieht@gmail.com>
2023-01-18 19:11:08 +0100

    Day 10: custom initializers

--- MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
@@ -694,3 +694,17 @@ app.contacts.append("Adrian E")
 app.contacts.append("Allen W")
 app.contacts.append("Ish S")
 
+// custom initializers
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+
+
