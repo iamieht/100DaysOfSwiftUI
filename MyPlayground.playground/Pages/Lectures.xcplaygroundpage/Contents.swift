f3e2e7afeabf797e35c7f5cbbb16aaba523a14d8
Iván E. Hernández T <iamieht@gmail.com>
2023-01-19 17:33:23 +0100

    Day 11

--- MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
@@ -707,4 +707,65 @@ struct Player {
 
 let player = Player(name: "Megan R")
 
+// ############ DAY 11 ###############
+// Access control
+struct BankAccount {
+    private var funds = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds > amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
+
+// private: Don't let anything outside the struct use this
+// fileprivate: Don't let anything outside the current file use this
+// public: Let anyone, anywhere use this
+// private(set): anyone can read but only internal methods can write
+
+// static properties and methods
+
+struct School {
+    static var studentCount = 0
+    
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Taylor Swift")
+print(School.studentCount)
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+struct Employee3 {
+    let username: String
+    let password: String
+    
+    static let example = Employee3(username: "cfederighi", password: "h4irf0rce0ne")
+}
 
