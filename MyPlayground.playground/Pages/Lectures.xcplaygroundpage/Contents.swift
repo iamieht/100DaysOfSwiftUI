fcfe1e393db3062c0e95a481655ae9148636c297
Iván E. Hernández T <iamieht@gmail.com>
2023-01-21 15:15:29 +0100

    Day 13: How to create and use protocols

--- MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Lectures.xcplaygroundpage/Contents.swift
@@ -884,3 +884,66 @@ class UserClass3 {
 let user8 = UserClass3()
 user8.name = "Taylor"
 print(user8.name)
+
+// ############## DAY 13 ###############
+// How to create and use protocols
+protocol Vehicle2 {
+    var name: String { get }
+    var currentPassengers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+    
+}
+
+struct Car2: Vehicle2 {
+    let name = "Car"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+    
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km")
+    }
+    
+    func openSunroof() {
+        print("It's a nice day")
+    }
+}
+
+struct Bicycle: Vehicle2 {
+    let name = "Bicycle"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+    
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km")
+    }
+}
+
+func commute(distance: Int, using vehicle2: Vehicle2) {
+    if vehicle2.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle")
+    } else {
+        vehicle2.travel(distance: distance)
+    }
+}
+
+func getTravelEstimates(using vehicles: [Vehicle2], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
+    }
+}
+
+let car = Car2()
+commute(distance: 100, using: car)
+
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
+getTravelEstimates(using: [car, bike], distance: 150)
