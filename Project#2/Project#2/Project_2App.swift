7f05727f48ab639103ef30553c4c665d324cbc8a
Iván E. Hernández T <iamieht@gmail.com>
2023-02-03 06:42:11 +0100

    Project#2
    VStack & HStack

---
+++ Project#2/Project#2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project#2
+//
+//  Created by Ivan E. Hernandez T on 03.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
