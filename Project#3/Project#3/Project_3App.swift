1ba2f30743952399466c514069571272952165fa
Iván E. Hernández T <iamieht@gmail.com>
2023-02-06 19:34:50 +0100

    Project#3
    Initial commit

---
+++ Project#3/Project#3/Project_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3App.swift
+//  Project#3
+//
+//  Created by Ivan E. Hernandez T on 06.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Project_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
